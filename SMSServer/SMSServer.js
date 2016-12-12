4f5887077ec12722d5ee86e94ecdd90f5e1493fb
Luis Leon <lleon95@gmail.com>
2016-12-12 19:46:51 +0100

    Add files via upload

--- SMSServer/SMSServer.js
+++ SMSServer/SMSServer.js
@@ -10,9 +10,16 @@ var SIM900 = new SerialPort('/dev/ttyS0', {
 	baudRate: 9600
 });
 
+
+SIM900.on('open', function() {
+  console.log("SIM900 Port Open");
+  SERVER_INIT();
+});
+
 // Initialization procedure
-function()
+function SERVER_INIT()
 {
+	console.log("Starting SIM900..");
 	SIM900_INIT();
 }
 
@@ -34,9 +41,9 @@ function SIM900_INIT()
 {
 	// Verifying if SIM900 has SMS capability
 	SIM900.write("AT+CMGF=1\r\n");
-	while(true)
-		if(incomingSerial.indexOf("OK") != -1)
-			break;
+	//while(true)
+	//	if(incomingSerial.indexOf("OK") != -1)
+	//		break;
 	// Verifying if SIM900 has SMS settings right
 	SIM900.write("AT+CSMP?\r\n");
 
@@ -47,20 +54,22 @@ function SIM900_INIT()
 		waiting = false;
 	},1000);
 
-	while(waiting)
-		if(incomingSerial.indexOf("+CSMP: 17,167,0,0") != -1)
-			settingsOK = true;
+	//while(waiting)
+	//	if(incomingSerial.indexOf("+CSMP: 17,167,0,0") != -1)
+	//		settingsOK = true;
 	// Correcting settings
 	if(!settingsOK)
 		SIM900.write("AT+CSMP=17,167,0,0\r\n");
-	while(true)
-		if(incomingSerial.indexOf("OK") != -1)
-			break;
+	//while(true)
+	//	if(incomingSerial.indexOf("OK") != -1)
+	//		break;
 	// Sending message to Admin
+	console.log("Sending a message to admin..");
 	SIM900.write("AT+CMGS="+AdminNumber+"\r\n");
-	while(true)
-		if(incomingSerial.indexOf(">") != -1)
-			break;
+	//while(true)
+	//	if(incomingSerial.indexOf(">") != -1)
+	//		break;
 	SIM900.write("Dear "+AdminName+". The SMS Server has been initialized correctly."+Submit);
+    console.log("SIM900's been started..");
 }
 
